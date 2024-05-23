eecde5a5606184fbd6777463383ebcfc42ef85e4
yayo1983 <yazanenator@gmail.com>
2024-05-23 13:15:37 -0600

    agregado comentarios a main y rutas

--- app/main.py
+++ app/main.py
@@ -6,7 +6,14 @@ from app.routes.vehicle_route import VehicleRouter
 
 
 class MyApp:
+    """
+    MyApp class encapsulates the setup and configuration of the FastAPI application.
+    """
     def __init__(self):
+        """
+        Initializes the MyApp instance by creating database tables, setting up the FastAPI app,
+        configuring CORS, and including routers for various routes.
+        """
         # Create all tables
         Base.metadata.create_all(bind=engine)
 
@@ -28,6 +35,10 @@ class MyApp:
         self._include_routers()
 
     def _include_routers(self):
+        """
+        Includes the service vehicle and service order routers in the FastAPI application
+        with specific prefixes.
+        """
         self.app.include_router(
             self.service_vehicle_router.router, prefix="/api/v1/vehicles"
         )
@@ -36,6 +47,15 @@ class MyApp:
         )
 
     def _setup_cors(self):
+        """
+        Configures Cross-Origin Resource Sharing (CORS) middleware for the FastAPI application.
+        This allows the application to handle requests from different origins, which is useful 
+        for frontend-backend communication during development.
+
+        Note:
+            - In this setup, all origins, headers, and methods are allowed, which is convenient for development
+            but should be restricted in a production environment for security reasons.
+        """
         # Allow all origins, headers, and methods (be careful with this in production)
         self.app.add_middleware(
             CORSMiddleware,
--- app/routes/service_order_route.py
+++ app/routes/service_order_route.py
@@ -8,11 +8,22 @@ from typing import List
 
 
 class ServiceOrderRouter:
+    """
+    ServiceOrderRouter class manages the routing for service order operations in the FastAPI application.
+    It defines the routes and their corresponding handler methods for creating, reading, updating, and 
+    deleting service orders.
+    """
     def __init__(self):
+        """
+        Initializes the ServiceOrderRouter instance by creating an APIRouter and setting up the routes.
+        """
         self.router = APIRouter()
         self._setup_routes()
 
     def _setup_routes(self):
+        """
+        Sets up the routes for the service order operations and links them to their respective handler methods.
+        """
         self.router.post("/", response_model=ServiceOrderRead)(
             self.create_service_order
         )
@@ -30,16 +41,50 @@ class ServiceOrderRouter:
     def create_service_order(
         self, service_order: ServiceOrderCreate, db: Session = Depends(get_db)
     ):
+        """
+        Handles the creation of a new service order.
+
+        Args:
+            service_order (ServiceOrderCreate): The data required to create a new service order.
+            db (Session): The database session dependency.
+
+        Returns:
+            ServiceOrderRead: The created service order.
+        """
         presenter = ServiceOrderPresenter(db)
         return presenter.create_service_order(service_order)
 
     def get_service_orders(
         self, skip: int = 0, limit: int = 30, db: Session = Depends(get_db)
     ):
+        """
+        Retrieves a list of service orders with pagination.
+
+        Args:
+            skip (int): The number of records to skip (default is 0).
+            limit (int): The maximum number of records to return (default is 30).
+            db (Session): The database session dependency.
+
+        Returns:
+            List[ServiceOrderRead]: A list of service orders.
+        """
         presenter = ServiceOrderPresenter(db)
         return presenter.get_service_orders(skip, limit)
 
     def get_service_order(self, order_id: int, db: Session = Depends(get_db)):
+        """
+        Retrieves a specific service order by its ID.
+
+        Args:
+            order_id (int): The ID of the service order to retrieve.
+            db (Session): The database session dependency.
+
+        Returns:
+            ServiceOrderRead: The requested service order.
+
+        Raises:
+            HTTPException: If the service order is not found.
+        """
         presenter = ServiceOrderPresenter(db)
         try:
             return presenter.get_service_order(order_id)
@@ -52,6 +97,20 @@ class ServiceOrderRouter:
         service_order: ServiceOrderUpdate,
         db: Session = Depends(get_db),
     ):
+        """
+        Updates an existing service order by its ID.
+
+        Args:
+            order_id (int): The ID of the service order to update.
+            service_order (ServiceOrderUpdate): The new data for the service order.
+            db (Session): The database session dependency.
+
+        Returns:
+            ServiceOrderRead: The updated service order.
+
+        Raises:
+            HTTPException: If the service order is not found.
+        """
         presenter = ServiceOrderPresenter(db)
         try:
             return presenter.update_service_order(order_id, service_order)
@@ -59,6 +118,19 @@ class ServiceOrderRouter:
             raise HTTPException(status_code=404, detail="Service order not found")
 
     def delete_service_order(self, order_id: int, db: Session = Depends(get_db)):
+        """
+        Deletes a service order by its ID.
+
+        Args:
+            order_id (int): The ID of the service order to delete.
+            db (Session): The database session dependency.
+
+        Returns:
+            None: Indicates successful deletion.
+
+        Raises:
+            HTTPException: If the service order is not found.
+        """
         presenter = ServiceOrderPresenter(db)
         try:
             return presenter.delete_service_order(order_id)
